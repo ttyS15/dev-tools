c93d6bf5238efffd9df1ff4a52af850cf1501863
Alex Manaev <ttyS15@yandex.ru>
2012-07-11 02:51:06 +0400

    svn-release.sh - tool for automated build of release tag

--- bin/svn-release.sh
+++ bin/svn-release.sh
@@ -3,83 +3,79 @@
 # $Id: $
 
 #
-# Выполняет создание тега сборки релиза. 
-# 
-# Example: svn-release.sh 3.x 3.2.0 'SQR-123 - Сборка релиза' http://svn.rutube.ru/squirrel 
-# 
+# Выполняет создание тега сборки релиза.
+#
+# Example: svn-release.sh 3.x 3.2.0 'SQR-123 - Сборка релиза' http://svn.rutube.ru/squirrel
+#
 # PROJEECT_URL указывать не обязательно, если команда выполняется из какой-либо рабочей копии проекта.
 #
 
 STABLE=$1
 RELEASE=$2
-MESSAGE_HEADER=$3
-PROJECT_URL=${4:-^}
+MESSAGE_HEADER=${3:-'Automated release builder'}
+PROJECT_URL="${4:-^}"
+
+STABLE_BASE="$PROJECT_URL/branches/stable"
+RELEASE_BASE="$PROJECT_URL/tags/release"
+
 
 if [ -z "$STABLE" -o -z "$RELEASE" -o -z "$MESSAGE_HEADER" ]; then
 	echo
 	echo "Use:"
-	echo "	$0 STABLE RELEASE MESSAGE_HEADER [ PROJECT_URL ]"
+	echo "	$0 STABLE RELEASE [ MESSAGE_HEADER [ PROJECT_URL ] ]"
 	echo
 	echo "Example:"
 	echo "	$0 3.x 3.2.0 'SQR-123 - Сборка релиза' http://svn.rutube.ru/squirrel"
 	echo
-	exit 127
+	exit -1
 fi
 
-STABLE_URL="$PROJECT_URL/branches/stable/$STABLE"
+STABLE_URL="$STABLE_BASE/$STABLE"
 RELEASE_URL="$PROJECT_URL/tags/release/$RELEASE"
 
-echo
-echo "STABLE URL = $STABLE_URL"
-echo "RELEASE URL = $RELEASE_URL"
-echo "MESSAGE_HEADER = $MESSAGE_HEADER"
-echo
+RELEASE_DIR=`svn ls $RELEASE_BASE | grep -E "^$RELEASE/"`
 
-#if [ -n "$MESSAGE_HEADER" ]; then 
-#	echo "$MESSAGE_HEADER" > commit-message.txt
-#else
-#	echo -n > commit-message.txt
-#fi
-#
-#echo >> commit-message.txt
-#echo "Request revisions from $SOURCE:" >> commit-message.txt
-#echo >> commit-message.txt
-#
-#svn up
-#
-#TASKS=`echo "$TASKS" | sed -e 's/,/|/'`
-#
-#svn log -l 100 "$SOURCE" | grep -E "^($TASKS)" -B2 | awk -F '|' ' NR%4==1 {system("echo -n " $1 " >> commit-message.txt "); rev=substr($1,2); print rev;} NR%4==3 {system("echo \" \"" $0 " >> commit-message.txt");}' | sort -n | xargs -Irev svn merge -crev $SOURCE
-#
-#echo
-#echo "COMMIT MESSAGE"
-#echo 
-#cat commit-message.txt
-#echo
-#
-#COMMIT_COMMAND="svn ci -F commit-message.txt"
-#
-#
-#echo "COMMIT COMMAND"
-#echo
-#echo "$COMMIT_COMMAND"
-#echo 
-#
-#while [ 1 ]; do
-#	echo -n 'Do you have fastened the seat belt? (yes/no) [no]:'
-#	read DO_IT_MOTHER_FUCKER
-#
-#	if [ "$DO_IT_MOTHER_FUCKER" = 'yes' ]; then 
-#		echo "God bless you! Go..."
-#		`$COMMIT_COMMAND`
-#		exit 0
-#	elif [ -z "$DO_IT_MOTHER_FUCKER" -o "$DO_IT_MOTHER_FUCKER" = 'no' ]; then
-#		echo "Looser!"
-#		exit 127
-#	fi
-#
-#	echo
-#	echo "This is your last chance! What is your choice: yes or no?"
-#	echo 
-#
-#done
+if [ -z $RELEASE_DIR ]; then
+	svn mkdir -m "$MESSAGE_HEADER : Create dir for release $RELEASE" $RELEASE_URL || exit -1
+	BUILD_URL="$RELEASE_URL/01"
+else
+	# Получаем номер крайнего билда в релизе и увеичиваем на 1
+	BUILD=`svn ls $RELEASE_URL | grep -E '[0-9]{2}/' | tail -1 | colrm 3`
+	BUILD=$((BUILD+1))
+	BUILD=`printf '%02d' $BUILD`
+fi
+
+BUILD_URL="$RELEASE_URL/$BUILD"
+
+COMMAND="svn cp $STABLE_URL $BUILD_URL -m '$MESSAGE_HEADER : Create release build $RELEASE-$BUILD'"
+
+echo
+echo "Ok, you are going to do this shit:"
+echo 
+echo "	$COMMAND"
+echo
+echo "Do you really know what do you do?"
+echo "If so, just type 'yes' and we will see do you have nuts such stronger as you think."
+echo -n "So, what is your bet? (yes/[no])"
+
+while [ 1 ]; do
+	
+	read DO_IT_MOTHER_FUCKER
+
+	if [ "$DO_IT_MOTHER_FUCKER" = 'yes' ]; then
+		echo
+		echo "Hold on to your chair! Go..."
+		$COMMAND || exit -1
+		echo
+		exit 0
+	elif [ -z "$DO_IT_MOTHER_FUCKER" -o "$DO_IT_MOTHER_FUCKER" = 'no' ]; then
+		echo
+		echo "Stay away from men's work!"
+		echo
+		exit -1
+	fi
+
+	echo
+	echo -n "What I see? Are you real strong man with big balls or just pretty virgin? Lets check? (yes/[no])"
+
+done
