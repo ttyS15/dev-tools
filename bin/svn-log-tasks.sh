4c525c3219026f994c7cc449b7f81dd624abe723
Sergey Tikhonov <zimbler@gmail.com>
2013-03-06 19:42:37 +0400

    svn-log-tasks

--- bin/svn-log-tasks.sh
+++ bin/svn-log-tasks.sh
@@ -5,10 +5,11 @@
 #
 # Выполняет поиск коммитов начиная с указанной ревизии
 #
-# Example: svn-log-tasks.sh 200 FH
+# Example: svn-log-tasks.sh 200 FH ^/trunk
 
 START_REVISION=$1
 PROJECT_TITLE=$2
+BRANCH=$3
 
 if [ -z "$START_REVISION" -o -z "$PROJECT_TITLE" ]; then
 	echo
@@ -25,4 +26,6 @@ echo "---------------------------"
 echo
 echo "Found $PROJECT_TITLE tasks:"
 echo
-svn log -r $1:HEAD|grep $2-|awk '{print $1}'|sort -u
+svn log -r $1:HEAD $BRANCH|grep $2-|awk '{print $1}'|sort -u
+echo
+echo "That's all folks!"
